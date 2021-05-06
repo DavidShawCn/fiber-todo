6d3c7cb7ae71aad6a2ef6358d65cef9c7ec2a6f7
DavidShawn <shadowshao@126.com>
2021-05-06 17:11:57 +0800

    init test3

--- src/com/pt/java/HelloWorld.java
+++ src/com/pt/java/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world haha2222");
         System.out.println("hello world haha1111");
+        System.out.println("hello world haha1111");
+        System.out.println("hello world haha1111");
 
     }
 }
