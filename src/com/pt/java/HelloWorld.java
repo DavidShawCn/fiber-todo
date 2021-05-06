4bcca76620c9d22fd2ca4875235ccb0cd32cddf6
DavidShawn <shadowshao@126.com>
2021-05-06 17:10:18 +0800

    init test

---
+++ src/com/pt/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.pt.java;
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world haha");
+        System.out.println("hello world haha1111");
+
+    }
+}
